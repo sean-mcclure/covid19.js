86b6ef7e52b74e49e30a66376533e5e5390cdd3f
Sean McClure <seanmcclure@Seans-MacBook-Pro.local>
2020-03-16 18:18:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,181 @@
+# covid19.js
+
+The **covid19.js** library can be used to fetch daily updated data from the COVID Data [Repository](https://github.com/CSSEGISandData/COVID-19) by **Johns Hopkins CSSE**. The library provides a set of useful functions for fetching both daily report and time series data since recording started.
+
+## Methods with Examples
+
+### get_all_countries
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+
+#### returns
+ - @returns {array} (all countries listed in COVID Data)
+
+#### example
+    covid.get_all_countries("deaths")
+
+### get_all_country_states
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+ -  @param {string} **country** (choose *country*)
+
+#### returns
+ - @returns {array} (all countries associated with chosen country)
+
+#### example
+    covid.get_all_country_states("deaths", "US")
+
+### check_if_country_has_state
+#### parameters
+ -  @param {string} **country** (choose *country*)
+
+#### returns
+ - @returns {boolean} (true/false whether chosen country has associated states)
+
+#### example
+    covid.check_if_country_has_state("Italy")
+
+### find_time_series_with_state
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+ - @param {string} **state** (choose *state*)
+ - @param {string} **country** (choose *country*)
+
+#### returns
+ - @returns {object} (time series object for chosen state)
+
+#### example
+    covid.find_time_series_with_state("recovered", "California", "US")
+
+### get_report_by_country_and_state
+#### parameters
+ - @param {string} **date** (choose report *date*)
+ - @param {string} **country** (choose *country*)
+ - @param {string} **state** (choose *state*)
+
+#### returns
+ - @returns {object} (summary object with state total *deaths*, *confirmed*, *recovered*)
+
+#### example
+    covid.get_report_by_country_and_state("03-06-2020", "Canada", "British Columbia")
+
+#### output
+
+    {deaths: "0", confirmed: "21", recovered: "3"}
+
+### get_global_report
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+
+#### returns
+ - @returns {object} (summary object with global total *deaths*, *confirmed*, *recovered*)
+
+#### example
+    covid.get_global_report("recovered")
+
+#### output
+
+    76034
+
+### sort_totals_by_country
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+
+#### returns
+ - @returns {object} (sorted object by country with choice values)
+
+#### example
+    covid.sort_totals_by_country("recovered")
+
+#### output
+
+    {
+        China:  67017,
+        Iran:  4590,
+        Italy:  2335,
+        Spain:  517,
+        Korea South:  510,
+        Cruise Ship:  325,
+        Japan:  118,
+        Singapore:  105,
+        Bahrain:  60,
+        Germany:  46,
+    ...
+
+### sort_totals_by_state
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+
+#### returns
+ - @returns {object} (sorted object by states with choice values)
+
+#### example
+    covid.sort_totals_by_state("deaths")
+
+#### output
+
+    {
+        Hubei:  3085,
+        Italy:  1809,
+        Iran:  724,
+        Spain:  289,
+        France:  91,
+        Korea South:  75,
+        Washington:  40,
+        Henan:  22,
+    ...
+
+### get_longs_and_lats
+#### parameters
+ - @param {string} **choice** (choose between *deaths*, *confirmed*, *recovered*)
+
+#### returns
+ - @returns {array} (array of objects with longitudes and latitudes for every country/state)
+
+#### example
+    covid.get_longs_and_lats("confirmed")
+
+#### output
+
+    [
+        {state:  "Hubei",  country:  "China",  longitude:  "112.2707 ",  latitude:  "30.9756"}
+        {state:  "",  country:  "Italy",  longitude:  "12.5674 ",  latitude:  "41.8719"}
+        {state:  "",  country:  "Iran",  longitude:  "53.688 ",  latitude:  "32.4279"}
+        {state:  "",  country:  ""Korea South"",  longitude:  "127.7669 ",  latitude:  "35.9078"}
+    ...
+
+### get_states_and_countries
+#### returns
+ - @returns {array} (array of objects with all state and country pairs)
+
+#### example
+    covid.get_states_and_countries()
+
+#### output
+
+      [
+           {country:  "China",  state:  "Hubei"}
+           {country:  "Italy",  state:  ""}
+           {country:  "Iran",  state:  ""}
+           {country:  ""Korea South"",  state:  ""}
+           {country:  "Spain",  state:  ""}
+           {country:  "Germany",  state:  ""}
+           {country:  "France",  state:  "France"}
+           ...
+
+### get_country_from_state
+#### parameters
+ - @param {string} **state** (choose *state*)
+ -
+#### returns
+ - @returns {string} (name of country associated with state)
+
+#### example
+    covid.get_country_from_state("Faroe Islands")
+
+#### output
+
+    "Denmark"
+
+## Support
+Reach out to [Sean McClure](https://twitter.com/sean_a_mcclure) if you need assistance or would like to see additional useful functions added to this library.
---
+++ covid19.js
@@ -0,0 +1,372 @@
+covid = {
+    "hold_value": hold_value = {},
+    "hold_value.corona_dates" : hold_value.corona_dates = [],
+    "hold_value.delay_cnt" : hold_value.delay_cnt = -1,
+    "dedupe_array": function dedupe_array(arr) {
+        new_arr = arr.filter(function(item, pos) {
+            return arr.indexOf(item) == pos;
+        })
+        return (new_arr)
+    },
+    "remove_from_array": function remove_from_array(arr, elem) {
+        new_arr = arr.filter(function(x) {
+            return (x !== elem)
+        })
+        return (new_arr)
+    },
+    "sort_object": function sort_object(obj) {
+        items = Object.keys(obj).map(function(key) {
+            return [key, obj[key]];
+        });
+        items.sort(function(first, second) {
+            return second[1] - first[1];
+        });
+        sorted_obj = {}
+        Object.keys(items).forEach(function(elem, i) {
+            var k = Object.keys(items)[i]
+            var v = Object.values(items)[i]
+            use_key = v[0]
+            use_value = v[1]
+            sorted_obj[use_key] = use_value
+        })
+        return (sorted_obj)
+    },
+    "get_everything_after": function get_everything_after(str, character_choice) {
+        new_str = str.split(character_choice).pop();
+        return (new_str)
+    },
+    "call_callback": function call_callback(cb) {
+        cb
+    },
+    "call_once_satisfied": function call_once_satisfied(props) {
+        if (eval(props['condition'])) {
+            if (typeof(props.function) == 'function') {
+                covid.call_callback(props.function())
+            } else {
+                eval(props['function'])
+            }
+        } else {
+            setTimeout(function() {
+                call_once_satisfied(props)
+            }, 100)
+        }
+    },
+    "delay_multiple": function delay_multiple(options) {
+        var index = 0;
+        function delayed_loop() {
+            setTimeout(function() {
+                if (typeof(options.function) == 'function') {
+                    covid.call_callback(options.function())
+                } else {
+                    eval(options['function'])
+                }
+                index++;
+                if (index < options['iterations']) {
+                    delayed_loop();
+                }
+            }, options['delay'])
+        }
+        delayed_loop();
+    },
+    "urls": urls = {
+        "time_series_confirmed": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+        "time_series_deaths": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+        "time_series_recovered": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv",
+        "daily_reports": "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/XX-XX-XXXX.csv"
+    },
+    "clean_first_commas_from_states": function clean_first_commas_from_states(data) {
+        var res = []
+        var lines = data.split("\n")
+        lines.forEach(function(line) {
+            if (line !== "") {
+                if (line.split(",")[0].includes('"')) {
+                    var first_comma_removed = line.replace(",", " ")
+                    res.push(first_comma_removed)
+                } else {
+                    res.push(line)
+                }
+            }
+        })
+        return (res)
+    },
+    "clean_commas_from_countries": function clean_commas_from_countries(data) {
+        var res = data.split("Korea, South").join("Korea South")
+        return (res)
+    },
+    "convert_fetched_csv_to_json": function convert_fetched_csv_to_json(data) {
+        var cleaned_countries = covid.clean_commas_from_countries(data)
+        var use_data = covid.clean_first_commas_from_states(cleaned_countries)
+        var res = []
+        use_data.forEach(function(elem, i) {
+            var inner = {}
+            if (i === 0) {
+                header_elems = elem.split(",")
+            } else {
+                elem.split(",").forEach(function(data_point, j) {
+                    inner[header_elems[j].trim()] = data_point
+                })
+                res.push(inner)
+            }
+        })
+        return (res)
+    },
+    "convert_json_to_line_data": function convert_json_to_line_data(data) {
+        var outer = []
+        date_cnt = 0
+        data.forEach(function(elem) {
+            var inner = []
+            Object.keys(elem).forEach(function(look_for_date, i) {
+                if (look_for_date.includes("/") && !look_for_date.includes("Province") && !look_for_date.includes("State") && !look_for_date.includes("Country")) {
+                    date_cnt++
+                    inner_inner = {}
+                    inner_inner["index"] = date_cnt
+                    if (Math.sign(Number(Object.values(elem)[i])) < 0) {
+                        inner_inner["value"] = 0
+                    } else {
+                        inner_inner["value"] = Number(Object.values(elem)[i])
+                    }
+                    inner_inner["true_date"] = look_for_date //.split("/").join("-")
+                    inner.push(inner_inner)
+                } else {
+                    if (look_for_date.includes("Country/Region")) {
+                        inner.push(elem["Country/Region"].split('"').join('').trim())
+                    }
+                    if (look_for_date.includes("Province")) {
+                        inner.push(elem["Province/State"].split('"').join('').trim())
+                    }
+                }
+            })
+            outer.push(inner)
+            date_cnt = 0
+        })
+        return (outer)
+    },
+    "get_all_countries": function get_all_countries(choice) {
+        var res = []
+        covid.fetch_results[choice].forEach(function(elem, i) {
+            res.push(elem[1])
+        })
+        return (covid.dedupe_array(res))
+    },
+    "get_all_country_states": function get_all_country_states(choice, country) {
+        var res = []
+        covid.fetch_results[choice].forEach(function(elem, i) {
+            if (elem[1] === country) {
+                res.push(elem[0])
+            }
+        })
+        return (res)
+    },
+    "check_if_country_has_state": function check_if_country_has_state(country) {
+        var check = covid.get_all_country_states("deaths", country)
+        if (check.length === 0 || check[0] === "") {
+            var res = false
+        } else {
+            var res = true
+        }
+        return (res)
+    },
+    "find_time_series_with_state": function find_time_series_with_state(choice, state, country) {
+        var res = {}
+        covid.fetch_results[choice].forEach(function(elem) {
+            if (elem[0] === state && elem[1] === country) {
+                var res_temp = covid.remove_from_array(elem, country)
+                res[elem[0]] = covid.remove_from_array(res_temp, state)
+            }
+            if (elem[0] === "" && elem[1] === country) {
+                var res_temp = covid.remove_from_array(elem, country)
+                res[elem[1]] = covid.remove_from_array(res_temp, "")
+            }
+        })
+        return (res)
+    },
+    "get_data_dates": function get_data_dates(data) {
+        var temp_dates = covid.remove_from_array(covid.get_everything_after(data.split('\n')[0], "Long").split(","), "")
+        temp_dates.forEach(function(elem) {
+            var pieces = elem.split("/")
+            if (pieces[0].length < 2) {
+                var new_month = "0" + pieces[0]
+            } else {
+                var new_month = pieces[0]
+            }
+            if (pieces[1].length < 2) {
+                var new_day = "0" + pieces[1]
+            } else {
+                var new_day = pieces[1]
+            }
+            var new_year = pieces[2].replace("20", "2020")
+            covid.hold_value.corona_dates.push(new_month.trim() + "-" + new_day.trim() + "-" + new_year.trim())
+        })
+    },
+    "get_report_by_country_and_state": function get_report_by_country_and_state(date, country, state) {
+        var res = {}
+        covid.fetch_results_reports[date].forEach(function(elem) {
+            if (elem["Country/Region"] === country && elem["Province/State"] === state) {
+                res["deaths"] = elem["Deaths"]
+                res["confirmed"] = elem["Confirmed"]
+                res["recovered"] = elem["Recovered"]
+            }
+        })
+        return (res)
+    },
+    "get_global_report": function get_global_report(choice) {
+        // since cumulative, only need most recent report
+        var use_choice = choice.charAt(0).toUpperCase() + choice.slice(1)
+        var global_deaths = []
+        var most_recent_report = covid.fetch_results_reports[covid.hold_value.corona_dates[covid.hold_value.corona_dates.length - 1].trim()]
+        most_recent_report.forEach(function(elem) {
+            if (elem[use_choice] !== "" && typeof(elem[use_choice]) !== 'undefined') {
+                global_deaths.push(Number(elem[use_choice]))
+            }
+        })
+        return (global_deaths.reduce((a, b) => a + b, 0))
+    },
+    "order_totals_by_country": function order_totals_by_country(choice) {
+        // since cumulative, only need most recent report
+        var use_choice = choice.charAt(0).toUpperCase() + choice.slice(1)
+        var res = {}
+        var most_recent_report = covid.fetch_results_reports[covid.hold_value.corona_dates[covid.hold_value.corona_dates.length - 1].trim()]
+        most_recent_report.forEach(function(elem) {
+            if (elem[use_choice] !== "" && typeof(elem[use_choice]) !== 'undefined') {
+                if (Object.keys(res).includes(elem["Country/Region"])) {
+                    res[elem["Country/Region"]].push(Number(elem[use_choice]))
+                } else {
+                    res[elem["Country/Region"]] = [Number(elem[use_choice])]
+                }
+            }
+        })
+        return (res)
+    },
+    "order_totals_by_state": function order_totals_by_state(choice) {
+        // since cumulative, only need most recent report
+        var use_choice = choice.charAt(0).toUpperCase() + choice.slice(1)
+        var res = {}
+        var most_recent_report = covid.fetch_results_reports[covid.hold_value.corona_dates[covid.hold_value.corona_dates.length - 1].trim()]
+        most_recent_report.forEach(function(elem) {
+            if (elem[use_choice] !== "" && typeof(elem[use_choice]) !== 'undefined') {
+                if (Object.keys(res).includes(elem["Province/State"])) {
+                    if (elem["Province/State"] === "") {
+                        elem["Province/State"] = elem["Country/Region"]
+                        res[elem["Province/State"]].push(Number(elem[use_choice]))
+                    } else {
+                        res[elem["Province/State"]].push(Number(elem[use_choice]))
+                    }
+                } else {
+                    if (elem["Province/State"] === "") {
+                        elem["Province/State"] = elem["Country/Region"]
+                        res[elem["Province/State"]] = [Number(elem[use_choice])]
+                    } else {
+                        res[elem["Province/State"]] = [Number(elem[use_choice])]
+                    }
+                }
+            }
+        })
+        return (res)
+    },
+    "sort_totals_by_country": function sort_totals_by_country(choice) {
+        var totals = covid.order_totals_by_country(choice)
+        res = {}
+        Object.keys(totals).forEach(function(elem, i) {
+            res[elem] = Object.values(totals)[i].reduce((a, b) => a + b, 0)
+        })
+        var res_sorted = covid.sort_object(res)
+        return (res_sorted)
+    },
+    "sort_totals_by_state": function sort_totals_by_state(choice) {
+        var totals = covid.order_totals_by_state(choice)
+        res = {}
+        Object.keys(totals).forEach(function(elem, i) {
+            res[elem] = Object.values(totals)[i].reduce((a, b) => a + b, 0)
+        })
+        var res_sorted = covid.sort_object(res)
+        return (res_sorted)
+    },
+    "get_longs_and_lats": function get_longs_and_lats(choice) {
+        var res = []
+        var most_recent_report = covid.fetch_results_reports[covid.hold_value.corona_dates[covid.hold_value.corona_dates.length - 1].trim()]
+        var all_deaths = []
+        var all_confirmed = []
+        var all_recovered = []
+        most_recent_report.forEach(function(elem) {
+            all_deaths.push(elem.Deaths)
+            all_confirmed.push(elem.Confirmed)
+            all_recovered.push(elem.Recovered)
+        })
+        most_recent_report.forEach(function(elem, i) {
+            var inner = {}
+            inner.state = elem["Province/State"]
+            inner.country = elem["Country/Region"]
+            inner.longitude = elem["Longitude"]
+            inner.latitude = elem["Latitude"]
+            res.push(inner)
+        })
+        return (res)
+    },
+    "fetch_corona_data": function fetch_corona_data(choice) {
+        if (choice == "confirmed") {
+            var url = covid.urls.time_series_confirmed
+        }
+        if (choice == "deaths") {
+            var url = covid.urls.time_series_deaths
+        }
+        if (choice == "recovered") {
+            var url = covid.urls.time_series_recovered
+        }
+        fetch(url).then(response => response.text()).then(data => {
+            covid.get_data_dates(data)
+            var use_data = covid.convert_fetched_csv_to_json(data)
+            covid.fetch_results[choice] = covid.convert_json_to_line_data(use_data)
+        });
+    },
+    "fetch_corona_reports": function fetch_corona_reports(date) {
+        var url = urls.daily_reports.replace("XX-XX-XXXX", date)
+        fetch(url).then(response => response.text()).then(data => {
+            covid.fetch_results_reports[date] = covid.convert_fetched_csv_to_json(data)
+        });
+    },
+    "get_states_and_countries": function get_states_and_countries() {
+        var use_choice = "Deaths"
+        var res = []
+        var most_recent_report = covid.fetch_results_reports[covid.hold_value.corona_dates[covid.hold_value.corona_dates.length - 1].trim()]
+        most_recent_report.forEach(function(elem) {
+            if (elem[use_choice] !== "" && typeof(elem[use_choice]) !== 'undefined') {
+                var inner = {}
+                inner.country = elem["Country/Region"]
+                inner.state = elem["Province/State"]
+                res.push(inner)
+            }
+        })
+        return (res)
+    },
+    "get_country_from_state": function get_country_from_state(state) {
+        var res = ""
+        var states_and_countries = covid.get_states_and_countries()
+        states_and_countries.forEach(function(elem) {
+            if (elem["state"] === state) {
+                res = elem["country"]
+            }
+            if (state === "Korea South") {
+                res = "Korea South"
+            }
+        })
+        return (res)
+    },
+    "fetch_results": fetch_results = {},
+    "fetch_results_reports": fetch_results_reportss = {}
+}
+covid.fetch_corona_data("confirmed", {})
+covid.fetch_corona_data("deaths", {})
+covid.fetch_corona_data("recovered", {})
+covid.call_once_satisfied({
+    "condition": "covid.hold_value.corona_dates.length > 0",
+    "function": function() {
+        covid.delay_multiple({
+            "delay": 40,
+            "iterations": covid.hold_value.corona_dates.length,
+            "function": function() {
+                covid.hold_value.delay_cnt++
+                covid.fetch_corona_reports(covid.hold_value.corona_dates[covid.hold_value.delay_cnt].trim())
+            }
+        })
+    }
+})
